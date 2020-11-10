119e46c44a60879738da1b37ff9ea57fd640a5dc
lizitong67 <820908499@qq.com>
2020-11-10 21:15:33 +0800

    anomaly detection toy

--- unsupervised_graph_classification.py
+++ unsupervised_graph_classification.py
@@ -7,24 +7,24 @@ Date: 2020.10.25
 
 import os
 import dgl
-import dgl.function as fn
 import numpy as np
-import networkx as nx
+import matplotlib.pyplot as plt
 import time
 import torch as th
 import torch.nn as nn
-import torch.nn.functional as F
-from dgl.nn.pytorch import GraphConv
 from dgl.data import DGLDataset
 from torch.utils.data import *
 from torch.utils.data import DataLoader
-from dgi import DGI, Classifier
+from dgi import DGI
 from anomaly_detection import Autoencoder
 
+
+
 # Customized Dataset
-class MyDataset(DGLDataset):
+class Normal_Dataset(DGLDataset):
     def __init__(self):
-        super().__init__(name="MyDataset")  # 调用父类构造方法
+        super(Normal_Dataset, self).__init__(name='Normal_Dataset', verbose=True)  # 调用父类构造方法
+
 
     def has_cache(self):
         return True
@@ -32,20 +32,45 @@ class MyDataset(DGLDataset):
     def load(self):
         self.graph_list = []
         self.label_list = []
-
-        homograph = "dataset/homograph"
-        scenarios = os.listdir(homograph)
+        normal_scenario = "dataset/homograph/normal"
+        scenarios = os.listdir(normal_scenario)
         for scenario in scenarios:
-            file_path = "dataset/homograph/" + scenario
+            file_path = "dataset/homograph/normal/" + scenario
             graphs = os.listdir(file_path)
             for graph in graphs:
                 g_list, label_dict = dgl.load_graphs(file_path + '/' + graph)
                 self.graph_list.append(g_list[0])
-                for key, value in label_dict.items():
-                    if key != 'Drive-by-download':
-                        self.label_list.append(0)
-                    else:
-                        self.label_list.append(1)
+                self.label_list.append(1)
+
+    def __getitem__(self, idx):
+        """
+         Get graph and label by index
+        """
+        return self.graph_list[idx], th.tensor(self.label_list[idx])
+
+    def __len__(self):
+        """Number of graphs in the dataset"""
+        return len(self.graph_list)
+
+class Attack_Dataset(DGLDataset):
+    def __init__(self):
+        super(Attack_Dataset, self).__init__(name="Attack_Dataset", verbose=True)  # 调用父类构造方法
+
+    def has_cache(self):
+        return True
+
+    def load(self):
+        self.graph_list = []
+        self.label_list = []
+        attack_scenario = "dataset/homograph/attack"
+        scenarios = os.listdir(attack_scenario)
+        for scenario in scenarios:
+            file_path = "dataset/homograph/attack/" + scenario
+            graphs = os.listdir(file_path)
+            for graph in graphs:
+                g_list, label_dict = dgl.load_graphs(file_path + '/' + graph)
+                self.graph_list.append(g_list[0])
+                self.label_list.append(0)
 
     def __getitem__(self, idx):
         """
@@ -65,52 +90,72 @@ def collate(batch):
     return batched_graph, batched_labels
 
 def graph_embedding():
+    # Training the graph embedding model
     # create DGI model
     dgi = DGI(in_feats,
               n_hidden,
               dropout)
     dgi_optimizer = th.optim.Adam(dgi.parameters())
 
-    # train DGI model for generate the embedding
-    dur = []
-
     for epoch in range(100):
         dgi.train()
         t0 = time.time()
         loss_list = []
         for batched_graph, labels in train_dataloader:
-            feats = batched_graph.ndata['feat'].float()
-            loss = dgi(batched_graph, feats)
+            features = batched_graph.ndata['feat'].float()
+            loss = dgi(batched_graph, features)
             dgi_optimizer.zero_grad()
             loss.backward()
             dgi_optimizer.step()
             loss_list.append(loss.item())
-        avg_loss = np.mean(loss_list)
-        dur.append(time.time() - t0)
-        print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f}".format
-             (epoch, np.mean(dur), avg_loss))
+        print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f}".format(epoch, time.time()-t0, np.mean(loss_list)))
     th.save(dgi.state_dict(), 'best_dgi.pkl')
-    print("[+] The best training model has been saved.")
+    print("[+] The best graph embedding model has been saved.")
 
-def classify():
-    # dataset
-    glist, label_dict = dgl.load_graphs("dataset/homograph/YouTube/0.bin")
-    g = glist[0]
-
-    # get graph embedding
+def train_autoencoder():
     dgi = DGI(in_feats,
               n_hidden,
               dropout)
     dgi.load_state_dict(th.load('best_dgi.pkl'))
-    feature = g.ndata['feat'].float()
-    g_embedding = dgi.encoder(g, feature)
 
-    # anomaly detection
-    model = Autoencoder()
+    autoencoder = Autoencoder()
     criterion = nn.MSELoss()
-    optimizer = th.optim.Adam(model.parameters())
+    optimizer = th.optim.Adam(autoencoder.parameters())
 
-    # continue in from here
+    for epoch in range(30):
+        autoencoder.train()
+        t0 = time.time()
+        loss_list = []
+        for batched_graph, labels in train_dataloader:
+            features = batched_graph.ndata['feat'].float()
+            # get graph embedding
+            g_embedding = dgi.encoder(batched_graph, features)
+            outputs = autoencoder(g_embedding)
+            loss = criterion(outputs, g_embedding)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_list.append(loss.item())
+        print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f}".format(epoch, time.time()-t0, np.mean(loss_list)))
+    th.save(autoencoder.state_dict(), 'best_autoencoder.pkl')
+    print("[+] The best autoencoder classifier model has been saved.")
+
+def classification():
+    dgi = DGI(in_feats, n_hidden, dropout)
+    classifier = Autoencoder()
+    dgi.load_state_dict(th.load('best_dgi.pkl'))
+    classifier.load_state_dict(th.load('best_autoencoder.pkl'))
+    criterion = nn.MSELoss()
+    loss_label = []
+    for graphs, labels in test_dataloader:
+        features = graphs.ndata['feat'].float()
+        g_embedding = dgi.encoder(graphs, features)
+        outputs = classifier(g_embedding)
+        loss = criterion(outputs, g_embedding)
+        # print(loss)
+        # tensor(0.0957, grad_fn=<MeanBackward0>)
+        loss_label.append([loss.item(), labels.item()])
+    print(loss_label)
 
 if __name__ == "__main__":
     # initial parameters
@@ -118,14 +163,16 @@ if __name__ == "__main__":
     n_hidden = 20
     dropout = 0
     patience = 20
-
-    # split dataset
-    dataset = MyDataset()
     batch_size = 16
+
+    # load dataset
     train_split = 0.8
-    train_size = int(train_split * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    normal_dataset = Normal_Dataset()
+    attack_dataset = Attack_Dataset()
+    train_size = int(train_split * len(normal_dataset))
+    test_size_normal = len(normal_dataset) - train_size
+    train_dataset, test_dataset_normal = random_split(normal_dataset, [train_size, test_size_normal])
+    test_dataset = ConcatDataset([attack_dataset, test_dataset_normal])
 
     train_dataloader = DataLoader(
         train_dataset,
@@ -140,5 +187,7 @@ if __name__ == "__main__":
         drop_last=False,
         shuffle=True)
     # graph_embedding()
-    # classify()
+    # train_autoencoder()
+    classification()
+
 
